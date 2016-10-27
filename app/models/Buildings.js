0880f593c3b0e98d11b149d6584070542768b330
Goupilleau, Alex Pascal <agoupilleau3@gatech.edu>
2016-10-27 18:41:11 -0400

    third commit

--- app/models/Buildings.js
+++ app/models/Buildings.js
@@ -1,11 +1,9 @@
 var mongoose = require('mongoose');
 
-/*var BuildingSchema = new mongoose.Schema({
+var BuildingSchema = new mongoose.Schema({
   title: String,
   cost: Number
 });
 
 var Building = mongoose.model('Building', BuildingSchema);
 
-var sc = new Building({ name: 'Student Center' });
-console.log(sc.name); */
